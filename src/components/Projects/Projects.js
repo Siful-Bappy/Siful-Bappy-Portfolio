874c77edae5c7bfbf5ccf57a7e616615401b9631
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-06-24 21:32:46 +0900

    two project is been uploaded

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
+import emaJhon from "../../Assets/Projects/Ema JhonJPG.JPG";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import bitsOfCode from "../../Assets/Projects/Smart Home project.jpg";
 
 function Projects() {
   return (
@@ -35,19 +35,19 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="Smart Home"
+              description="Smart Home Application was a group project where used raspberry pi with different different sensors and can control those sensors by the web app. Supports voice command."
+              link="https://github.com/Siful-Bappy/smart-home-application"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={emaJhon}
               isBlog={false}
-              title="Editor.io"
+              title="Ema Jhon"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              link="https://github.com/Siful-Bappy/daraz-eCommerce"
             />
           </Col>
 
