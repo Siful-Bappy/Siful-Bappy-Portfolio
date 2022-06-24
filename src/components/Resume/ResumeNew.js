f85c4831672bcb5a5b7b75f0a14e93ad0e54a9b8
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-06-24 18:29:20 +0900

    resume is updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,13 +4,17 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 // import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import pdf from "../../Assets/../Assets/Siful-Bappy-Resume.pdf";
+import resumeImg from "../../Assets/../Assets/Siful-Bappy.png";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "./ResumeNew.css"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Siful-Bappy-Resume.pdf";
+  "https://github.com/Siful-Bappy/Siful-Bappy-Portfolio/blob/master/src/Assets/Siful-Bappy-Resume.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -35,10 +39,13 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
+        {/* <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+        </Row> */}
+        <Row className="my-5 w-50 mx-auto">
+          <img src={resumeImg} alt="" />
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
